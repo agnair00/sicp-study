ce964d20a75e8491c4c626a0a3796fa5458542cf
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-10-16 13:14:12 +0530

    Added ex 1.30 solution

---
+++ solutions/1_30.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (identity x) x)
+(define (inc x) (+ 1 x))
+
+;test
+(sum identity 1 inc 5) ;result = 15
