1ef03c2f41c8b023d4705488c1796b9b8b0d1520
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-09 12:53:31 +0530

    Added solutions to ex 1.17 1.18

--- solutions/1_17.rkt
+++ solutions/1_17.rkt
@@ -3,11 +3,12 @@
 (define (double x) (* x 2))
 (define (halve x) (/ x 2))
 
-(define (fast-mult a b n)
-  (cond ((= n 0) a)
-        ((even? n) (fast-mult a (double b) (halve n)))
-        (else (fast-mult (+ a b) b (- n 1)))))
+(define (fast-mult a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (fast-mult a (halve b))))
+        (else (+ a (fast-mult a (- b 1))))))
 
 ;test
-(fast-mult 0 3 7)
-(fast-mult 0 9 4)
+(fast-mult 3 7)
+(fast-mult 9 4)
+(fast-mult 4 0)
