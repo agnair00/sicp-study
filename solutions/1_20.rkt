d2f2e1698574e6ef4885953afb83e3c9b161d106
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-10 12:45:24 +0530

    Added soln to ex 1.20

---
+++ solutions/1_20.rkt
@@ -0,0 +1,20 @@
+;normal-order evaluation
+;(gcd 206 40)
+;(gcd 40 (remainder 206 40))
+;(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+;(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+;(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+;Number of remainder operations is 18
+
+;applicative-order evaluation
+;(gcd 206 40)
+;(gcd 40 (remainder 206 40))
+;(gcd 40 6)
+;(gcd 6 (remainder 40 6))
+;(gcd 6 4)
+;(gcd 4 (remainder 6 4))
+;(gcd 4 2)
+;(gcd 2 (remainder 4 2))
+;(gcd 2 0)
+;2
+;Number of remainder operations is 4
