282c6e216dad7b899102a704bb8fadadbbb1a749
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-10 11:37:13 +0530

    Modified sol of ex 1.18 and added sol to ex 1.19

--- solutions/1_18.rkt
+++ solutions/1_18.rkt
@@ -3,11 +3,12 @@
 (define (double x) (* x 2))
 (define (halve x) (/ x 2))
 
-(define (fast-mult a b n)
-  (cond ((= n 0) a)
-        ((even? n) (fast-mult a (double b) (halve n)))
-        (else (fast-mult (+ a b) b (- n 1)))))
+(define (fast-mult q a b)
+  (cond ((= b 0) q)
+        ((even? b) (fast-mult q (double a) (halve b)))
+        (else (fast-mult (+ q a) a (- b 1)))))
 
 ;test
 (fast-mult 0 3 7)
 (fast-mult 0 9 4)
+(fast-mult 0 4 0)
