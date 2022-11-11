b25f79e46dc4ca0b4a274e53a6cc91cdb758a08d
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-11 17:13:23 +0530

    Added soln to ex 1.38

---
+++ solutions/1_38.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (cont-frac n d k)
+  (define (recur i)
+    (if (= i k)
+        (/ (n i) (d i))
+        (/ (n i) (+ (d i) (recur (+ i 1))))))
+  (recur 1))
+
+(define (d-euler i)
+  (if (= (modulo i 3) 2)
+      (* 2 (/ (+ i 1) 3))
+      1))
+
+;test
+(+ 2
+  (cont-frac (lambda (i) 1.0)
+             d-euler
+             100))
