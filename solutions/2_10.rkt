ae89d99dc8266e40a11e91c4aea47792f1e68010
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-16 08:26:49 +0530

    Added soln to ex 2.10

---
+++ solutions/2_10.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound i) (car i))
+(define (upper-bound i) (cdr i))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4)))) 
+
+(define (div-interval x y)
+  (cond ((or (zero? (lower-bound y)) (zero? (upper-bound y))) (error "Dividing by 0!" y))
+        ((and (negative? (lower-bound y)) (positive? (upper-bound y))) (error "Dividing by 0!!" y))
+        (else (mul-interval x
+                            (make-interval (/ 1.0 (upper-bound y))
+                                           (/ 1.0 (lower-bound y)))))))
+
+(define interval1 (make-interval 1.5 2.0))
+(define interval2 (make-interval -0.8 4.5))
+
+(mul-interval interval1 interval2)
+(div-interval interval1 interval2)
