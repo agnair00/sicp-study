76bc9fdf44364291c1aca7eee5390d4a4736eb7a
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-16 17:27:37 +0530

    Added soln to ex 2.12

---
+++ solutions/2_12.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound i) (car i))
+(define (upper-bound i) (cdr i))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (make-center-width c (* (/ p 100) c)))
+
+(define (percent i)
+  (* (/ (width i) (center i)) 100))
+
+;test
+(percent (make-center-percent 78 3))
