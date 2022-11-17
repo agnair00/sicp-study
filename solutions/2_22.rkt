7ebed37850db05d2272f3637dff7474d4cd5b7df
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-17 11:25:37 +0530

    Added soln to ex 2.22

---
+++ solutions/2_22.rkt
@@ -0,0 +1,2 @@
+;This is because we end up with a nested pair where the first element of the outermost pair is nil.
+;Whereas, a sequence is defined to be nested pair where the last element of the innermost pair is nil.
