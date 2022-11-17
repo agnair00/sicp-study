89740431aae399413c0d43daa8dfc7b1ba0170a6
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-17 11:10:01 +0530

    Added soln to ex 2.19

---
+++ solutions/2_19.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+;test
+(define us-coins (list 50 25 10 5 1))
+(define us-coins1 (list 25 50 10 5 1))
+(cc 100 us-coins)
+(cc 100 us-coins1)
