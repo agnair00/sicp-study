f3bf2d44f1f9e533356da12106fa4ca3d3a1bb7b
Abhilash G Nair <114045366+agnair00@users.noreply.github.com>
2022-11-30 18:19:39 +0530

    Added soln to ex 2.64

---
+++ solutions/2_64.rkt
@@ -0,0 +1,43 @@
+#lang sicp
+
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+;test
+(define ordered-set (list 1 3 5 7 9 11))
+(list->tree ordered-set)
+
+;a) partial-tree works by recursivesly splitting the list in three parts, the first part being the left branch,
+;the second part being the middle element and the last part being the right branch which is then cons'd with
+;remaining elements.
+;Tree producef for (1 3 5 7 9 11)
+;(5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+;b) The order of growth is O(n)
